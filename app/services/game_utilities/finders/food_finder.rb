6d5ebb74cca3fa73894fc1ae12c182fd63e59674
Chris Sweeney <chris.sweeney@shopify.com>
2020-10-01 11:03:15 -0400

    I forget, some changes and some yard annotations

--- app/services/game_utilities/finders/food_finder.rb
+++ app/services/game_utilities/finders/food_finder.rb
@@ -5,23 +5,26 @@ module GameUtilities
     class FoodFinder < Finder
       attr_accessor :food
 
+      # Identifies which directions will move towards food and gives back the "best moves first"
+      #
+      # @return
       def directions_to_food
         nearest_coords = food[nearest]
         nearest_x = nearest_coords['x'] <=> snek['head']['x']
         nearest_y = nearest_coords['y'] <=> snek['head']['y']
 
         if (nearest_x == 1 && nearest_y == 1) || (nearest_x == 1 && nearest_y == 0)
-          %w(right up down)
+          %w(right up down left)
         elsif nearest_x == 1 && nearest_y == -1
-          %w(right down up)
+          %w(right down up left)
         elsif (nearest_x == -1 && nearest_y == 1) || (nearest_x == -1 && nearest_y == 0)
-          %w(left up down)
+          %w(left up down right)
         elsif nearest_x == -1 && nearest_y == -1
-          %w(left down up)
+          %w(left down up right)
         elsif nearest_x == 0 && nearest_y == 1
-          %w(up down left)
+          %w(up down left right)
         elsif nearest_x == 0 && nearest_y == -1
-          %w(down up left)
+          %w(down up left right)
         end
       end
 
--- app/services/game_utilities/movement_planner.rb
+++ app/services/game_utilities/movement_planner.rb
@@ -4,8 +4,13 @@ module GameUtilities
   class MovementPlanner
     include ActiveModel::Model
 
+    MAX_STEPS = 2
+
     attr_accessor :board, :snek
 
+    # Sends back the direction to move to the controller
+    #
+    # @return [Hash] The hash necessary to send back to the server as a JSON payload.
     def move
       direction = pick_direction
 
@@ -16,6 +21,10 @@ module GameUtilities
 
     private
 
+    # Determines what moves are available and returns the best one
+    #
+    # @private
+    # @return [String] The decided upon direction to move (up, down, left, or right)
     def pick_direction
       moves = available_moves
 
@@ -24,6 +33,9 @@ module GameUtilities
       best_next_move(moves: moves)
     end
 
+    # Looks at all available moves and returns an array of the only valid ones OR up (if no other valid options)
+    #
+    # @return [Array] Array of Hashes containing directions and their coordinates
     def available_moves
       moves = coordinates_from_location(location: snek['head'])
       valid_moves = moves.reject { |move| !valid_move?(coords: moves.dig(move)) }
@@ -31,67 +43,83 @@ module GameUtilities
       valid_moves.any? ? valid_moves : no_valid_moves
     end
 
+    # A fallback method for if there are no valid moves. Logs and returns a default direction.
+    #
+    # @return [String] In the absence of any good directions to move, just go up.
     def no_valid_moves
-      Rails.logger.info('No good moves. Guess I\'ll die')
+      Rails.logger.info("\n\t[NO VALID MOVES]")
 
       'up'
     end
 
+    # Attempts to find the best next move by identifying which moves towards food are valid moves and then
+    # obtaining scores for them. Returns the best next move based on the highest scoring move.
+    #
+    # @param moves [Array] A list of available directions to move (previously determined to be valid and towards food)
+    # @return [String] The direction determined to have the highest movement score
     def best_next_move(moves:)
       moves_towards_food = Finders::FoodFinder.new(food: board['food'], snek: snek).directions_to_food
       good_moves = moves_towards_food.reject { |move| !moves.include?(move) }
-      best_moves = []
+      move_scores = Hash.new(0)
 
-      if good_moves.length >= 2
-        good_moves.reject do |move|
-          best_moves << move if safe_next_move?(move: move)
-        end
-      elsif good_moves.length == 1
-        return good_moves.sample
-      else
-        return no_valid_moves
+      good_moves.each do |move|
+        Rails.logger.info("[I'M CURRENTLY AT #{snek['head']}")
+        Rails.logger.info("[MOVING #{move} WILL TAKE ME TO #{move_coords(move: move)}")
+        move_scores[move] = score_paths(current_coords: move_coords(move: move))
+        # Rails.logger.info("\tTHAT SCORE WILL BE #{move_scores[move]}")
       end
 
-      best_moves.first
+      Rails.logger.info("[MOVEMENT SCORES] #{move_scores}")
+      move_scores.max_by { |_k, v| v }.first
     end
 
-    def safe_next_move?(move:)
-      location = move_coords(move: move)
-      next_coords = coordinates_from_location(location: location)
-
-      next_coords.each do |_move, coord|
-        return true if valid_move?(coords: coord)
-      end
-
-      false
-    end
-
-    def move_coords(move:)
+    # Gets the coordinates for the move given
+    #
+    # @param move [String] The direction of the move
+    # @return [Hash] The coordinates that the move will take the snek head to.
+    def move_coords(coords: snek['head'], move:)
       case move
       when 'up'
-        { 'x' => snek['head']['x'], 'y' => snek['head']['y'] + 1 }
+        { 'x' => coords['x'], 'y' => coords['y'] + 1 }
       when 'down'
-        { 'x' => snek['head']['x'], 'y' => snek['head']['y'] - 1 }
+        { 'x' => coords['x'], 'y' => coords['y'] - 1 }
       when 'left'
-        { 'x' => snek['head']['x'] - 1, 'y' => snek['head']['y'] }
+        { 'x' => coords['x'] - 1, 'y' => coords['y'] }
       when 'right'
-        { 'x' => snek['head']['x'] + 1, 'y' => snek['head']['y'] }
+        { 'x' => coords['x'] + 1, 'y' => coords['y'] }
       end
     end
 
+    # Checks to see if moving to the coordinates given can be considered valid by making sure the snek isn't eating
+    # itself, is still on the board, and isn't hitting another shake.
+    #
+    # @param coords [Hash] The x,y coordinates that are being validated
+    # @return [Boolean] The decision of if the move will result in death or not
     def valid_move?(coords:)
       still_on_board?(coords) && !eating_self?(coords) && !hitting_snake?(coords)
     end
 
+    # Looks to see if the coordinates are going to collide with any part of the snek body
+    #
+    # @param move_coords [Hash] The x,y coordinates that are being validated
+    # @return [Boolean] Is the snek going to take a chomp of itself?
     def eating_self?(move_coords)
       snek['body'].any?(move_coords)
     end
 
+    # Looks to see if the coordinates are going to take the snek off the board
+    #
+    # @param move_coords [Hash] The x,y coordinates that are being validated
+    # @return [Boolean] Is the snek wandering off the board?
     def still_on_board?(move_coords)
       move_coords['x'].between?(0, board['width'] - 1) &&
           move_coords['y'].between?(0, board['height'] - 1)
     end
 
+    # Looks to see if the coordinates are going to take the snek into another snake
+    #
+    # @param move_coords [Hash] The x,y coordinates that are being validated
+    # @return [Boolean] Is the snek going to hit an enemy?
     def hitting_snake?(move_coords)
       snakes = Finders::SnakeFinder.new(snakes: board['snakes'], snek: snek).locations
 
@@ -104,6 +132,10 @@ module GameUtilities
       false
     end
 
+    # Gets coordinates for all available directions that can be moved.
+    #
+    # @param location [Hash] The x,y coordinates of the snek head
+    # @return [Hash] Hash of Hashes where the key is a movement direction and the value is a Hash of x,y coordinates
     def coordinates_from_location(location:)
       {
           'up' =>  { 'x' => location['x'], 'y' => location['y'] + 1 } ,
@@ -111,6 +143,38 @@ module GameUtilities
           'left' => { 'x' => location['x'] - 1, 'y' => location['y'] },
           'right' => { 'x' => location['x'] + 1, 'y' => location['y'] },
       }
+    end
+
+    # Scores?
+    #
+    # @param step [Integer] The recursive depth counter
+    # @param current_coords [Hash] x,y coordinates of where the head _thinks_ it is
+    # @return [Integer] The score of the passed in move.
+    def score_paths(step: 0, current_coords:)
+      spacer = "--" * step
+      score = step
+      Rails.logger.info("#{spacer} | [THE SCORE RIGHT NOW IS #{score}]")
+      directions = Finders::FoodFinder.new(food: board['food'], snek: snek).directions_to_food
+
+      if step == MAX_STEPS
+        Rails.logger.info("#{spacer} | [MAX STEPS REACHED - RETURNING #{score}]")
+        return score
+      end
+
+      unless valid_move?(coords: current_coords)
+        Rails.logger.info("#{spacer} | [MOVE TO #{current_coords} IS INVALID -- RETURNING 0]")
+        return 0
+      end
+
+      directions.each do |direction|
+        next_coordinates = move_coords(coords: current_coords, move: direction)
+
+        Rails.logger.info("#{spacer} | [RECURSIVELY CHECKING MY MOVE #{direction} WHICH WILL TAKE ME TO #{next_coordinates}]")
+        score += score_paths(step: step + 1, current_coords: next_coordinates)
+      end
+
+      Rails.logger.info("#{spacer} | [RETURNING THE SCORE #{score}]")
+      score
     end
   end
 end
